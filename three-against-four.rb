f2336998db54c5b68320ab5cdda1f7e6ff482555
Jan Ole Hüser <j0le@users.noreply.github.com>
2022-05-28 00:16:43 +0200

    Make ratio variable

--- three-against-four.rb
+++ three-against-four.rb
@@ -12,9 +12,12 @@ define :my_play do |bpm, sample_sym, repeats|
 end
 
 
+high = 4
+low = 3
+
 state = 1
 start_bpm = 30.0
-end_bpm = start_bpm*4.0/3.0
+end_bpm = start_bpm*1.0*high/low
 bpm = start_bpm
 
 sound_a = :perc_snap
@@ -28,30 +31,31 @@ live_loop :arp do
     if state == 1
       
       if bpm == start_bpm
-        #sample :perc_bell, amp: 0.5
+        sample :perc_bell, amp: 0.5
       end
       
-      my_play bpm, sound_a, 4
-      my_play bpm, sound_b, 3
+      my_play bpm, sound_a, high
+      my_play bpm, sound_b, low
       
       
       if bpm >= end_bpm
         state = 2
         bpm = end_bpm
       else
-        bpm += start_bpm/15.0
+        bpm += start_bpm/(1.0*low*high)
       end
       
     elsif state == 2
+      sample :perc_bell, amp: 0.5
       sample sound_a
-      my_play bpm, sound_b, 3
+      my_play bpm, sound_b, low
       
       bpm = start_bpm
       state=3
       
     elsif state == 3
-      #sample :perc_bell, amp: 0.5
-      my_play bpm, sound_b, 4
+      sample :perc_bell, amp: 0.5
+      my_play bpm, sound_b, high
       
       # swap sounds
       helper = sound_a
