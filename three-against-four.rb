08d102e030a005cdc136fde459181ffd07a5599a
Jan Ole Hüser <j0le@users.noreply.github.com>
2022-05-28 00:04:41 +0200

    Make endless loop

--- three-against-four.rb
+++ three-against-four.rb
@@ -1,27 +1,72 @@
 load_sample :perc_snap2
 
-bpm = 30
+define :my_play do |bpm, sample_sym, repeats|
+  in_thread do
+    with_bpm bpm do
+      repeats.times do
+        sample sample_sym
+        sleep 1.0/repeats
+      end
+    end
+  end
+end
+
+
+state = 1
+start_bpm = 30.0
+end_bpm = start_bpm*4.0/3.0
+bpm = start_bpm
+
+sound_a = :perc_snap
+sound_b = :perc_snap2
+
 live_loop :arp do
   #loop do
-  use_bpm bpm
-  
-  in_thread do
-    use_bpm bpm
+  with_bpm bpm do
     
-    3.times do
-      sample :perc_snap
-      sleep 1/3.0
+    
+    if state == 1
+      
+      if bpm == start_bpm
+        #sample :perc_bell, amp: 0.5
+      end
+      
+      my_play bpm, sound_a, 4
+      my_play bpm, sound_b, 3
+      
+      
+      if bpm >= end_bpm
+        state = 2
+        bpm = end_bpm
+      else
+        bpm += start_bpm/15.0
+      end
+      
+    elsif state == 2
+      sample sound_a
+      my_play bpm, sound_b, 3
+      
+      bpm = start_bpm
+      state=3
+      
+    elsif state == 3
+      #sample :perc_bell, amp: 0.5
+      my_play bpm, sound_b, 4
+      
+      # swap sounds
+      helper = sound_a
+      sound_a = sound_b
+      sound_b = helper
+      
+      state=1
+    else
+      # alarm
+      my_play bpm*2, :perc_bell, 7
+      
     end
+    
+    sleep 1
   end
-  
-  in_thread do
-    use_bpm bpm
-    4.times do
-      sample :perc_snap2
-      sleep 1/4.0
-    end
-  end
-  sleep 1
-  bpm = bpm * 1.05
 end
+
 
